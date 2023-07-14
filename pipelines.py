1008a8bfdc174e2ffa359e7daa4e5e323df9e386
Berkin Akbiyik <121386664+berkin9@users.noreply.github.com>
2023-07-14 08:03:04 +0300

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -1,36 +1,54 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 import pymongo
+import sys
+from .items import StackItem
 
-from scrapy import settings
-from scrapy.exceptions import DropItem
-from scrapy import log
+class MongoDBPipeline:
 
+    collection = 'kitayurdu'
 
-class MongoDBPipeline(object):
+    def __init__(self, mongodb_uri, mongodb_db):
+        self.mongodb_uri = mongodb_uri
+        self.mongodb_db = mongodb_db
+        if not self.mongodb_uri: sys.exit("You need to provide a Connection String.")
 
-    def __init__(self):
-        connection = pymongo.MongoClient(
-            settings['MONGODB_SERVER'],
-            settings['MONGODB_PORT']
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongodb_uri=crawler.settings.get('MONGODB_URI'),
+            mongodb_db=crawler.settings.get('MONGODB_DATABASE', 'items')
         )
-        db = connection[settings['MONGODB_DB']]
-        self.collection = db[settings['MONGODB_COLLECTION']]
 
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongodb_uri)
+        self.db = self.client[self.mongodb_db]
+        # Start with a clean database
+        self.db[self.collection].delete_many({})
 
-def process_item(self, item, spider):
-        valid = True
-        for data in item:
-            if not data:
-                valid = False
-                raise DropItem("Missing {0}!".format(data))
-        if valid:
-            self.collection.insert(dict(item))
-            log.msg("Book added to MongoDB database!",
-                    level=log.DEBUG, spider=spider)
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        data = dict(StackItem(item))
+        self.db[self.collection].insert_one(data)
         return item
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongodb_uri=crawler.settings.get('MONGODB_URI'),
+            mongodb_db=crawler.settings.get('MONGODB_DATABASE', 'items')
+        )
+    
+    def __init__(self, mongodb_uri, mongodb_db):
+        self.mongodb_uri = mongodb_uri
+        self.mongodb_db = mongodb_db
+        if not self.mongodb_uri: sys.exit("You need to provide a MongoDB Connection String.")
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongodb_uri)
+        self.db = self.client[self.mongodb_db]
+        # Start with a clean database
+        self.db[self.collection].delete_many({})
+
+    def close_spider(self, spider):
+        self.client.close()
